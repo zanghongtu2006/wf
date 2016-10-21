20c91ed3c7fdf5898f2816d432da47fdc83aa5d8
hongtu_zang <zanghongtu2006@gmail.com>
2016-10-21 19:22:05 +0800

    init demo "customer service"

---
+++ src/main/java/com/hongtu/wf/demo/service/CustomerService.java
@@ -0,0 +1,32 @@
+package com.hongtu.wf.demo.service;
+
+import com.hongtu.wf.demo.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by hongtu on 2016/10/21.
+ */
+public class CustomerService {
+
+    public List<Customer> getCustomerList(String keyword) {
+        return null;
+    }
+
+    public Customer getCustomer(Long id) {
+        return null;
+    }
+
+    public boolean createCustomer(Map<String, Object> fields) {
+        return false;
+    }
+
+    public boolean updateCustomer(Long id, Map<String, Object> fields) {
+        return false;
+    }
+
+    public boolean deleteCustomer(Long id) {
+        return false;
+    }
+}
