00bd09f1e555ffd39a080cc6e000ae4b78f4aa7b
hongtu_zang <zanghongtu2006@gmail.com>
2016-11-04 19:12:01 +0800

    init jdbc static{code}

--- demo/src/main/java/com/hongtu/wf/demo/service/CustomerService.java
+++ demo/src/main/java/com/hongtu/wf/demo/service/CustomerService.java
@@ -1,14 +1,38 @@
 package com.hongtu.wf.demo.service;
 
 import com.hongtu.wf.demo.model.Customer;
+import com.hongtu.wf.demo.utils.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by hongtu on 2016/10/21.
  */
 public class CustomerService {
+    private static final Logger _logger = LoggerFactory.getLogger(CustomerService.class);
+
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWOD;
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWOD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            _logger.error("Can not load jdbc driver", e);
+        }
+    }
 
     public List<Customer> getCustomerList(String keyword) {
         return null;
--- demo/src/test/java/com/hongtu/wf/demo/CustomerServiceTest.java
+++ demo/src/test/java/com/hongtu/wf/demo/CustomerServiceTest.java
@@ -68,5 +68,4 @@ public class CustomerServiceTest {
         Assert.assertTrue(result);
     }
 
-
 }
