6d9993f9d0f8262daa77699949ca1549d4f43b25
zanghongtu2006 <zanghongtu2006@gmail.com>
2016-11-07 09:28:27 +0800

    modify web-demo project, delete unused files, make a class to load the config file "config.properties", add web app package path, db config, jsp file path, asset resource path

---
+++ demo/src/main/java/com/hongtu/wf/demo/helper/ConfigHelper.java
@@ -0,0 +1,41 @@
+package com.hongtu.wf.demo.helper;
+
+import com.hongtu.wf.demo.utils.ConfigConstant;
+import com.hongtu.wf.demo.utils.PropsUtil;
+
+import java.util.Properties;
+
+/**
+ * Created by hongtu on 16-11-7.
+ */
+public final class ConfigHelper {
+    private static final Properties CONFIG_PROPS = PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+
+    public static String getJdbcDriver() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_DRIVER);
+    }
+
+    public static String getJdbcUrl() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_URL);
+    }
+
+    public static String getJdbcUsername() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_USERNAME);
+    }
+
+    public static String getJdbcPassword() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_PASSWORD);
+    }
+
+    public static String getAppBasePackage() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_PACKAGE);
+    }
+
+    public static String getAppJspPath() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_JSP_PATH);
+    }
+
+    public static String getAppAssetPath() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_ASSET_PATH);
+    }
+}
---
+++ demo/src/main/java/com/hongtu/wf/demo/utils/ConfigConstant.java
@@ -0,0 +1,18 @@
+package com.hongtu.wf.demo.utils;
+
+/**
+ * Created by hongtu on 16-11-7.
+ */
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "config.properties";
+
+    String JDBC_DRIVER = "jdbc.driver";
+    String JDBC_URL = "jdbc.url";
+    String JDBC_USERNAME = "jdbc.username";
+    String JDBC_PASSWORD = "jdbc.password";
+
+    String APP_PACKAGE = "wf.app.base_package";
+    String APP_JSP_PATH = "wf.app.jsp_path";
+    String APP_ASSET_PATH = "wf.app.asset";
+}
