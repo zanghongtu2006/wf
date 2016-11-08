35e1b78e78b70664894df77d23e9fca40b988322
zanghongtu2006 <zanghongtu2006@gmail.com>
2016-11-08 23:52:21 +0800

    add 4 annotations:service,controller,action and inject

---
+++ wf/src/main/java/com/hongtu/wf/annotation/Controller.java
@@ -0,0 +1,14 @@
+package com.hongtu.wf.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by hongtu on 16-11-8.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+}
